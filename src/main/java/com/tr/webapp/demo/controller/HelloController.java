6100cd94778be8a01ce28acfbaeb0ae35f35d396
Tirthankar Barari <tirtho@hotmail.com>
2024-02-28 21:51:37 -0500

    added the sample code

---
+++ src/main/java/com/tr/webapp/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.tr.webapp.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RestController
+public class HelloController {
+     @GetMapping("Admin")
+     @ResponseBody
+     @PreAuthorize("hasAuthority('APPROLE_Admin')")
+     public String Admin() {
+         return "Admin message";
+     }
+}
